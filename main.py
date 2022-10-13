b69ed97f0080dc4a663de6e6174d22188a701082
Jordan Lee <67548512+RomanticCoder@users.noreply.github.com>
2022-10-13 17:36:34 -0400

    refactor - save files

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-from reprlib import recursive_repr
-from requests import get
-from bs4 import BeautifulSoup
-
 from extractors.wwr import extract_wwr_jobs
 from extractors.linkedIn import extract_linkedIn_jobs
+from file import save_to_file
+
 print("\nWhat kind of job are you looking for? \n")
 keyword = input("type the keyword: ")
-
-file = open(f"{keyword}_jobs.csv", "w")
-
-file.write("Position, Company, Location, URL\n")
 
 linkedIn = extract_linkedIn_jobs(keyword)
 wwr = extract_wwr_jobs(keyword)
 jobs = linkedIn + wwr
 
-for job in jobs:
-  file.write(f"{job['position']},{job['company']},{job['region']},{job['link']}\n" )
+save_to_file(keyword, jobs)
